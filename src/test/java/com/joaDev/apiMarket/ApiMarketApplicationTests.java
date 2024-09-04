0d37b27ae65067c07aa715c3da3f98bc11d65ef7
Joaquin Navarro <contactojoaquin517@gmail.com>
2024-09-04 09:54:05 -0300

    first commit

---
+++ src/test/java/com/joaDev/apiMarket/ApiMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaDev.apiMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
